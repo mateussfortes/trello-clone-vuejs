93a18989ef88bd7eed0872658b0f4ee8d461bb99
Mateus Fortes <mateussilveirafortes@gmail.com>
2020-06-26 00:13:47 -0300

    Add tailwind and make column dragable

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -6,9 +6,12 @@
                 class="column" 
                 v-for="(column, $columnIndex) of board.columns" 
                 :key="$columnIndex"
-                @drop="moveTask($event, column.tasks)"
+                draggable
+                @drop="moveTaskOrColumn($event, column.tasks, $columnIndex)"
                 @dragover.prevent
                 @dragenter.prevent
+                @dragstart.self="pickupColumn($event, $columnIndex)"
+
             >
                 <div class="flex items-center mb-2 font-bold">
                     {{ column.name }} 
@@ -21,6 +24,7 @@
                         @click="goToTask(task)"
                         draggable
                         @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
+                        @drop.stop="moveTaskOrColumn($event, column.tasks, $columnIndex, $taskIndex)"
                     >
                         
                         <span class="w-full flex-no-shrink font-bold">
@@ -88,16 +92,41 @@ export default {
 
             e.dataTransfer.setData('task-index', taskIndex)
             e.dataTransfer.setData('from-column-index', columnIndex)
+            e.dataTransfer.setData('type', 'task')
         },
-        moveTask(e, toTasks) {
+        pickupColumn(e, fromColumnIndex) {
+            e.dataTransfer.effectAllowed = 'move'
+            e.dataTransfer.dropEffect = 'move'
+
+            e.dataTransfer.setData('from-column-index', fromColumnIndex)
+            e.dataTransfer.setData('type', 'collumn')
+        },
+        moveTaskOrColumn(e, toTasks, toColumnIndex, toTaskIndex) {
+            const type = e.dataTransfer.getData('type')
+            if(type === 'task') {
+                this.moveTask(e, toTasks, toTaskIndex !== undefined ? toTaskIndex : toTaskIndex.length)
+            } else {
+                this.moveColumn(e, toColumnIndex)
+            }
+
+        },
+        moveColumn(e, toColumnIndex) {
+            const fromColumnIndex = e.dataTransfer.getData('from-column-index')
+            this.$store.commit('MOVE_COLUMN', {
+                fromColumnIndex,
+                toColumnIndex
+            })
+        },
+        moveTask(e, toTasks, toTaskIndex) {
             const fromColumnIndex = e.dataTransfer.getData('from-column-index')
             const fromTasks = this.board.columns[fromColumnIndex].tasks
-            const taskIndex = e.dataTransfer.getData('task-index')
+            const fromTaskIndex = e.dataTransfer.getData('from-task-index')
 
             this.$store.commit('MOVE_TASK', {
                 fromTasks,
+                fromTaskIndex,
                 toTasks,
-                taskIndex
+                toTaskIndex
             })
 
         }
